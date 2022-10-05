cdaa05c26847d86514e37cc3e030445e404df7da
kkumartp <113323001+kkumartp@users.noreply.github.com>
2022-10-05 18:53:29 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "B:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "B:\\Flutter\\Flutter Interview test\\eIRED\\todoapp" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=B:\\Flutter\\flutter"
+  "PROJECT_DIR=B:\\Flutter\\Flutter Interview test\\eIRED\\todoapp"
+  "FLUTTER_ROOT=B:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=B:\\Flutter\\Flutter Interview test\\eIRED\\todoapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=B:\\Flutter\\Flutter Interview test\\eIRED\\todoapp"
+  "FLUTTER_TARGET=B:\\Flutter\\Flutter Interview test\\eIRED\\todoapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=B:\\Flutter\\Flutter Interview test\\eIRED\\todoapp\\.dart_tool\\package_config.json"
+)
